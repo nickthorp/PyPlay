9241d17137cafea76da4b16d73a810a1f80bc2b6
Nick Thorp <nickthorp20@gmail.com>
2015-07-24 14:01:07 -0500

    Added some code

--- RPiStuff/gpiostuff.py
+++ RPiStuff/gpiostuff.py
@@ -5,3 +5,11 @@ try:
 except RuntimeError:
     print("Error importing RPi.GPIO! This is probably from permission access.")
 
+GPIO.setmode(GPIO.BCM)
+
+# Add a new channel for output to the led
+GPIO.setup(23, GPIO.OUT)
+
+GPIO.output(23, GPIO.HIGH)
+
+GPIO.cleanup()
